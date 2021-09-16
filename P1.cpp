0326bd3daa7d74d3f76cc98ff0a44a25dc6b18e5
wonyoungseok <yij3491@gmail.com>
2021-09-16 18:06:51 +0900

    추가

---
+++ P1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>  //기본 입출력 함수를 사용
+
+using namespace std; // 기본 namespace 설정
+
+int main()
+{
+    cout << "*****" << endl;
+    cout << "****" << endl;
+    cout << "***" << endl;
+    cout << "**" << endl;
+    cout << "*" << endl;
+
+    //추가
+    return 0;
+}
