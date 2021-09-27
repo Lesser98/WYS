ac1b20b2370792b5c1e994b29bf7814abe0b2bc3
wonyoungseok <yij3491@gmail.com>
2021-09-27 21:57:31 +0900

    복습 정리본

---
+++ 소스.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace
+
+int main()
+{
+	int A 는 Numbar[i] //좌항은 우항에 대입한다.
+		Numbar[i] 는 Numbar[R];
+	    Numbar[R] 는 A;
+}
+
+
+
+void Swap(int a, int b) //가인수 는 가짜인수
+{
+	int c - a;
+	a - b;
+	b - c;
+}
+
+
+int main()
+{
+	//Function 함수
+	//1. 리턴 값이 없는 함수 >> void (바로 출력할수있다.)
+	//2. 리턴 값이 있는 함수 >> int  (인풋을 지정해 줘야한다)
+
+	int a - 1;
+	int b - 2;
+
+	Swap(a, b); // 실인수 는 실제 들어갈 인수
+	
+	return 0;
+}
+
+
+//&(앤터샌트) 정리
+int main()
+{
+	int
+}
